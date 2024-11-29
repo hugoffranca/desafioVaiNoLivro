477a1c9d633a1e12d2609617157250e82d4d5e15
hugoffranca <alheiodef@live.com>
2024-11-29 14:58:41 -0300

    feat: finalizando page Livros Doados

--- src/pages/livrosDoados/LivrosDoados.jsx
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -1,8 +1,19 @@
+import LivroProtagonista from "../../assets/livroProtagonista.png";
 import s from "../livrosDoados/livrosDoados.module.scss";
 export default function LivrosDoados() {
   return (
-    <section>
-      <h1>teste1</h1>
+    <section className={s.livrosDoadosSection}>
+      <h2>Livros Doados</h2>
+      <section className={s.containerCards}>
+        <section>
+          <img src={LivroProtagonista} alt="Imagem do livro 'O protagonista'" />
+          <div>
+            <h3>O Protagonista</h3>
+            <p>Susane Andrade</p>
+            <p>Ficção</p>
+          </div>
+        </section>
+      </section>
     </section>
   );
 }
