00f06ab54a5d7b487a05a647a9af25b821e468c4
hugoffranca <alheiodef@live.com>
2025-03-28 15:58:38 -0300

    feat: integração /doar com BD

--- src/pages/queroDoar/QueroDoar.jsx
+++ src/pages/queroDoar/QueroDoar.jsx
@@ -1,22 +1,73 @@
 import s from "../queroDoar/queroDoar.module.scss";
 import iconeForm from "../../assets/iconeForm.png";
+import axios from "axios";
+import { useState } from "react";
+
 export default function QueroDoar() {
+  const [titulo, setTitulo] = useState("");
+  const [categoria, setCategoria] = useState("");
+  const [autor, setAutor] = useState("");
+  const [image_url, setImage_url] = useState("");
+
+  const capturaTitulo = (e) => {
+    setTitulo(e.target.value);
+  };
+  const capturaCategoria = (e) => {
+    setCategoria(e.target.value);
+  };
+  const capturaAutor = (e) => {
+    setAutor(e.target.value);
+  };
+
+  const capturaImage_url = (e) => {
+    setImage_url(e.target.value);
+  };
+
+  const enviarDados = async () => {
+    const dadosEnviar = {
+      titulo,
+      categoria,
+      autor,
+      image_url,
+    };
+
+    try {
+      await axios.post("https://api-vainolivro.onrender.com/doar", dadosEnviar);
+      alert("Dados enviados com sucesso!");
+    } catch (error) {
+      console.error("Erro ao enviar os dados:", error);
+      alert("Ocorreu um erro ao enviar os dados. Por favor, tente novamente.");
+    }
+  };
   return (
     <section className={s.queroDoarSection}>
       <p>
         Por favor, preencha o formulário com suas informações e as informações
         do Livro
       </p>
-      <form action="">
+      <form onSubmit={(e) => e.preventDefault()}>
         <div>
           <img src={iconeForm} alt="Icone de livro aberto" />
           <h2>Informações do Livro</h2>
         </div>
-        <input type="text" name="" id="" placeholder="Título" />
-        <input type="text" name="" id="" placeholder="Categoria" />
-        <input type="text" name="" id="" placeholder="Autor" />
-        <input type="text" name="" id="" placeholder="Link da Imagem" />
-        <input type="submit" value="Doar" className={s.botaoDoar} />
+        <input type="text" placeholder="Título" onChange={capturaTitulo} />
+        <input
+          type="text"
+          placeholder="Categoria"
+          onChange={capturaCategoria}
+        />
+        <input type="text" placeholder="Autor" onChange={capturaAutor} />
+        <input
+          type="text"
+          placeholder="Link da Imagem"
+          onChange={capturaImage_url}
+        />
+        <input
+          type="submit"
+          value="Doar"
+          className={s.botaoDoar}
+          onClick={enviarDados}
+        />
       </form>
     </section>
   );
